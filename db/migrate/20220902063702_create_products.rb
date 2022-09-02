2767cc7d7e009bea2dd53698a0df668c3f5c4b7d
Alexander ADAM <alexander.adam@vade.io>
2022-09-02 08:49:02 +0200

    ignore migration

--- db/migrate/20220902063702_create_products.rb
+++ db/migrate/20220902063702_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-    create_table :products do |t|
-      t.string :name
-      t.string :tagline
+    # create_table :products do |t|
+    #   t.string :name
+    #   t.string :tagline
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
